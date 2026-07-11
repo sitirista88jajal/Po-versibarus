dba2b5c63ca33d1a60aaa3c45ee2b1692aaf73ca
sitirista88jajal <sitirista88@gmail.com>
2026-07-11 23:44:51 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
-// Website PO Kaos
-console.log("Website PO Kaos siap digunakan.");
+function kirimWA(){
+
+let nama = document.getElementById("nama").value;
+let wa = document.getElementById("wa").value;
+let ukuran = document.getElementById("ukuran").value;
+let warna = document.getElementById("warna").value;
+let jumlah = document.getElementById("jumlah").value;
+
+// GANTI DENGAN NOMOR ADMIN
+let admin = "6287736330469";
+
+let pesan =
+`Assalamu'alaikum Admin PO Kaos.
+
+Saya ingin memesan kaos.
+
+Nama : ${nama}
+No. WA : ${wa}
+Ukuran : ${ukuran}
+Warna : ${warna}
+Jumlah : ${jumlah}
+
+Mohon diproses. Terima kasih.`;
+
+let url = "https://wa.me/" + admin + "?text=" + encodeURIComponent(pesan);
+
+window.open(url,"_blank");
+
+}
